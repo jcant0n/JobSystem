524639973bb93624f0e91d7375fd8cf772d3cd75
jcant0n <javiercantonferrero@gmail.com>
2024-09-11 13:38:58 +0200

    Working on tests.

--- JobSystemTest/Program.cs
+++ JobSystemTest/Program.cs
@@ -6,39 +6,11 @@ namespace JobSystemTest
     {
         static async Task Main(string[] args)
         {
-            JobSystem jobSystem = new JobSystem(3);
-            var context = new JobSystem.Context();
+            Tests.BasicSecuential();
 
-            //Console.WriteLine("JobSystem execute test.");
+            Tests.BasicDispatch();
 
-            //for (int i = 0; i < 10; i++)
-            //{
-            //    jobSystem.Execute(context, (args) =>
-            //    {
-            //        var counter = context.PendingJobs;
-            //        Console.WriteLine($"Pending Jobs {counter}, Thread {Thread.CurrentThread.ManagedThreadId}");
-            //        Thread.Sleep(1000); // Simulate workload
-            //    });
-            //}
-
-            //jobSystem.Wait(context);
-
-            Console.WriteLine("JobSystem dispatch test.");
-            uint count = 1000;
-            jobSystem.Dispatch(context, count, 100, (args) =>
-            {
-                if(args.JobIndex % 100 == 0)
-                {
-                    var counter = context.PendingJobs;
-                    Console.WriteLine($"Pending Jobs {counter}, Thread {Thread.CurrentThread.ManagedThreadId}");
-                }
-                Thread.Sleep(1); // Simulate workload
-            });
-            jobSystem.Wait(context);
-
-            jobSystem.Dispose();
-            
-            Console.WriteLine("JobSystem has been stopped, Press any key to continue.");
+            Console.WriteLine("All JobSystem tests are finished, Press any key to continue.");
             Console.ReadKey();
         }
     }
---
+++ JobSystemTest/Tests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobSystemTest
+{
+    public class Tests
+    {
+        public static Stopwatch sw = new Stopwatch();
+
+        public static void BasicSecuential()
+        {
+            sw.Reset();
+
+            JobSystem jobSystem = new JobSystem(3);
+            var context = new JobSystem.Context();
+
+            Console.WriteLine("[BasicSecuential test]");
+
+            sw.Start();
+            uint count = 1000;
+            jobSystem.Dispatch(context, count, 100, (args) =>
+            {
+                if (args.JobIndex % 100 == 0)
+                {
+                    var counter = context.PendingJobs;
+                    Console.WriteLine($"Pending Jobs {counter}, Thread {Thread.CurrentThread.ManagedThreadId}");
+                }
+                Thread.Sleep(1); // Simulate workload
+            });
+
+            jobSystem.Wait(context);
+            sw.Stop();
+            Console.WriteLine($"[BasicSecuential test] - Time: {sw.ElapsedMilliseconds}");
+            jobSystem.Dispose();
+        }
+
+        public static void BasicDispatch()
+        {
+            sw.Reset();
+
+            JobSystem jobSystem = new JobSystem(3);
+            var context = new JobSystem.Context();
+
+            Console.WriteLine("JBasicDispatch test.");
+
+            sw.Start();
+            uint count = 1000;
+            jobSystem.Dispatch(context, count, 100, (args) =>
+            {
+                if (args.JobIndex % 100 == 0)
+                {
+                    var counter = context.PendingJobs;
+                    Console.WriteLine($"Pending Jobs {counter}, Thread {Thread.CurrentThread.ManagedThreadId}");
+                }
+                Thread.Sleep(1); // Simulate workload
+            });
+
+            jobSystem.Wait(context);
+            sw.Stop();
+
+            Console.WriteLine($"JBasicDispatch test - Time: {sw.ElapsedMilliseconds}");
+            jobSystem.Dispose();
+        }
+    }
+}
